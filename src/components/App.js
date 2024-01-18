4c270982f67035beb5a8546d28f10d45ba223663
riyazgithubavailable <riyazisheikh97@gmail.com>
2024-01-18 15:04:21 +0530

    text1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+//import { update } from "cypress/types/lodash";
 
 const App = () => {
+  const [text,setText]=useState("");
+  function updateText(e){
+    e.preventDefault();
+    setText( "Hello "+e.target.value+"!");
+  }
+ 
   return (
     <div>
-        {/* Do not remove the main div */}
+      <label>Enter your name:</label>
+      <br />
+        <input type="text" onChange={updateText}></input>
+             <p>{text}</p>
     </div>
   )
 }
